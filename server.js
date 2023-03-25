2da8eb7d6ca7eab91758fbcdc540ff0fd641f591
kvnkrr <121253179+kvnkrr@users.noreply.github.com>
2023-03-25 20:00:22 +0000

    uploaded BE Code

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
-var express = require("express");
+const express = require('express');
+const app = express();
 
-var app = express();
 
-app.use(express.static('public'));
+const https = require('https');
 
-//make way for some custom css, js and images test 
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
+function getFastComSpeed() {
+  return new Promise((resolve, reject) => {
+    https.get('https://fast.com/', (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        resolve(data);
+      });
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+}
 
-var server = app.listen(9005, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-}); 
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+app.get('/refuge', (req, res) => {
+  res.sendFile('./refugeSchedule.png', {root: __dirname});
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile('./homePage.png', {root: __dirname});
+});
+
+app.get('/ookla', (req, res) => {
+  res.sendFile('./homePage.png', {root: __dirname});
+});
+
+app.listen(2090, () => {
+  console.log('Server listening on port 2090');
+});
