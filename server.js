c710976bab8d276c0ffbae15fdafacbc214aad27
Kevin Kerr <kvnkerruk@gmail.com>
2023-12-25 22:42:31 +0000

    changed back to localhost accepting queries for now as Pi not avail.

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ function getFastComSpeed() {
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://raspberrypi44.duckdns.org:3000');
+    /*res.header('Access-Control-Allow-Origin', 'http://raspberrypi44.duckdns.org:3000'); */ 
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
